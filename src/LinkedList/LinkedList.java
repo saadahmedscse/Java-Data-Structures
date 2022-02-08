5e5b53d517950d2e2428e38ca07134bbee58b5e0
Saad Ahmed <saadahmedscse@gmail.com>
2022-02-08 17:16:39 +0600

    Linked List

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,4 +1,20 @@
 package LinkedList;
 
 public class LinkedList {
+
+    Node head;
+
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(String data){
+        //asf
+    }
 }
