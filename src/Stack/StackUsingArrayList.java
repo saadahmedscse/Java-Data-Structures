24c878f9aa502188724e1111eab0f1fd45daf669
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-26 22:39:46 +0600

    Stack using ArrayList

---
+++ src/Stack/StackUsingArrayList.java
@@ -0,0 +1,48 @@
+package Stack;
+
+import java.util.ArrayList;
+
+public class StackUsingArrayList {
+
+    public static class Stack{
+        private static final ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        public static void push(int data){
+            list.add(data);
+        }
+
+        public static int pop(){
+            if (isEmpty()){
+                return -1;
+            }
+
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        public static int peek(){
+            if (isEmpty()){
+                return -1;
+            }
+
+            return list.get(list.size()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+        Stack.push(4);
+
+        while (!Stack.isEmpty()){
+            System.out.println(Stack.peek());
+            Stack.pop();
+        }
+    }
+}
