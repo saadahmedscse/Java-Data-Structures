114a0e7bea1a5bb4a7622080e0298d21b1ba908a
Saad Ahmed <saadahmedscse@gmail.com>
2022-04-01 16:54:41 +0600

    Building Binary Tree

---
+++ src/Binary Tree/BinaryTree.java
@@ -0,0 +1,38 @@
+public class BinaryTree{
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTr{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+
+            if (nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+    public static void main(String[] args) {
+        
+        int nodes[] = {9, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+        Node root = BinaryTr.buildTree(nodes);
+        System.out.println(root.data);
+        System.out.println(root.left.data + " " + root.right.data);
+    }
+}
